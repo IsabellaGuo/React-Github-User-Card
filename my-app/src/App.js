7b0f6bbc7bb7dddd8cb29572ab09585904007951
IsabellaGuo <isabella.guofei@gmail.com>
2020-01-14 15:37:33 -0800

    constructor in App.js is done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Axios from 'axios';
+import UserCard from './Components/GithubUserCard';
 
+class App extends React.Components {
+    constructor (){
+        super()
+        this.state = {
+            name: '',
+            email: '',
+            login: '',
+            avatar: '',
+            followers: []
+        }
+    }
+
+    componentDidMount () {
+        
+    }
+}
 
 
 
